127af6b6cf82490c3e6d3a17e008551917afe758
Yuvaraj Prem Kumar <46146150+yuvaraj91@users.noreply.github.com>
2023-03-20 14:14:56 +0100

    Adding python resources

---
+++ playground/models/people_small.py
@@ -0,0 +1,17 @@
+import base64
+import pandas
+
+def encoding(x):
+    x = x.encode("ascii")
+    base64_bytes = base64.b64encode(x)
+    return base64_bytes.decode("ascii")
+    
+    
+def model(dbt, fal):
+    dbt.config(materialized="table")
+    
+    df = dbt.ref("people_raw")
+    
+    df['family_name'] = df['family_name'].apply(encoding)
+
+    return df
