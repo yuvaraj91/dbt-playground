726ba394726182b038fe85c013111437066d1ae1
Yuvaraj Prem Kumar <46146150+yuvaraj91@users.noreply.github.com>
2023-03-22 09:42:23 +0100

    Cleaning up repo

--- playground/models/people.py
+++ playground/models/people.py
@@ -10,7 +10,6 @@ def encoding(x):
 def model(dbt, fal):
     dbt.config(materialized="table")
     
-    #df = dbt.ref("people_raw")
     df = dbt.source('public', 'people_raw')
     
     df['family_name'] = df['family_name'].apply(encoding)
